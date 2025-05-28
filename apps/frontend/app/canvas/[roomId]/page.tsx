f46a4fd89d1855d79fbd299a3b4f01e6c2036a33
Ayush Verma <vermaayushbly@gmail.com>
2025-05-28 19:24:09 +0530

    fix: remove incorrect await from dynamic route param

--- apps/frontend/app/canvas/[roomId]/page.tsx
+++ apps/frontend/app/canvas/[roomId]/page.tsx
@@ -1,13 +1,13 @@
 import { RoomCanvas } from "@/app/components/RoomCanvas";
 
-export default async function CanvasPage({ params }: {
-    params: {
-        roomId: string
-       
-    }
+export default async function CanvasPage({
+  params,
+}: {
+  params: {
+    roomId: string;
+  };
 }) {
-    const roomId = (await params).roomId;
+  const roomId = params.roomId;
 
-    return <RoomCanvas roomId={roomId}/>
+  return <RoomCanvas roomId={roomId} />;
 }
-
